6f2a379f548badca2d25ebb7021d6989ae896c20
all141 <all141@pitt.edu>
2018-02-22 20:01:32 -0500

    Add files via upload

---
+++ block.rb
@@ -0,0 +1,22 @@
+#Austin Linder & Jacqueline Marx
+#Deliverable 3 CS 1632
+# Block Class
+# Holds all the values that a block  in a Billcoin blockchain has
+class Block
+	#Block Number
+	@block_number
+	
+	#Hash of the previous block in the chain
+	@previous_hash
+	
+	#Sequence of transactions in block
+	@transaction_sequence
+	
+	#Timestamp
+	@timestamp
+	
+	#Hash of current block
+	@current_hash
+	
+	
+end
---
+++ verifier.rb
@@ -0,0 +1,4 @@
+#Austin Linder & Jacqueline Marx
+#Deliverable 3 CS 1632
+
+#MAIN PROGRAM EXECUTION
